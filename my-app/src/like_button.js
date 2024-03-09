0408f0c7f1beabe2a9e87f85ceab53aec7ccd197
Vladsilav Filippov <vlad.filippov.tmn@gmail.com>
2024-03-09 15:30:03 +0600

    стратовые потуги работу с гит

---
+++ my-app/src/like_button.js
@@ -0,0 +1,45 @@
+/*import {useState} from "react";
+
+function MyButton({count, onClick}){
+
+    return(
+        <button onClick={onClick}>My button {count}</button>
+    )
+}*/
+
+
+/*const arrayFruit = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+]
+
+export default function MyApp(){
+
+    const listItem = arrayFruit.map(product =>
+        <li key={product.id}
+            style={{color: product.isFruit ? 'magenta' : 'darkgreen'}}>
+            {product.title}
+        </li>
+    )
+
+    return(
+        <ul>
+            {listItem}
+        </ul>
+    )
+}*/
+
+/*
+export default function MyApp(){
+    const [count, setCount] = useState(0)
+    function handeClick(){
+        setCount(count + 1)
+    }
+    return(
+        <>
+            <MyButton count={count} onClick={handeClick} />
+            <MyButton count={count} onClick={handeClick} />
+        </>
+    )
+}*/
